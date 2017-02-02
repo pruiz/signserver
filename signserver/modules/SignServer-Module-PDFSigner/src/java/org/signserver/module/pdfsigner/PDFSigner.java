e72582561756285604f06f9d8f6f13957cc571a8
Pablo Ruiz <pablo.ruiz@gmail.com>
2017-02-02 13:41:16 +0100

    Fix arithmetic calculation so instead of reducing by 85% we do increas estimated content size by 15%

--- signserver/modules/SignServer-Module-PDFSigner/src/java/org/signserver/module/pdfsigner/PDFSigner.java
+++ signserver/modules/SignServer-Module-PDFSigner/src/java/org/signserver/module/pdfsigner/PDFSigner.java
@@ -638,7 +638,7 @@ public class PDFSigner extends BaseSigner {
            		// try signing again
 			// In order to accomodate for servers returning responses of
 			// varying sizes, let's add a 15% to previous response's size.
-			return addSignatureToPDFDocument(params, pdfbytes, password, (int)(contentExact * 0.15));
+			return addSignatureToPDFDocument(params, pdfbytes, password, (int)(contentExact * 1.15));
         	} else {
         		// if we fail to get an accurate signature size on the second attempt, bail out (this shouldn't happen)
         		throw new SignServerException("Failed to calculate signature size");
