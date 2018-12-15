222f4f75b7063d27014ab058a618aff2670aedb7
netmackan <netmackan@91c8134f-2a44-0410-ba45-dce2f532dbc6>
2018-12-15 13:08:31 +0000

    DSSINTER-241: Patching DeployTools
    git-svn-id: https://signserver.svn.sourceforge.net/svnroot/signserver/trunk@9926 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ jenkins-files/pipelines/build_unit/deploytools/maven-manually-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install DeployTools parent POM
+DPL_VERSION=2.2.1-SNAPSHOT
+mvn -Dmaven.repo.local=${MAVEN_REPO_LOCAL} install:install-file -DgroupId=org.signserver.deploytools -DartifactId=DeployTools -Dversion=${DPL_VERSION} -Dpackaging=pom -Dfile=DeployTools-${DPL_VERSION}-pom.xml $@
+
+# Install the DeployTools artifacts from JARS
+# Based on post by David Blevins: https://www.mail-archive.com/users@maven.apache.org/msg91297.html
+for jar in ./DeployTools-Common-${DPL_VERSION}.jar ./DeployTools-Maven-${DPL_VERSION}.jar; do
+    pom=$(jar tvf $jar | grep pom.xml | perl -pe 's,.* ,,')
+    props=$(jar tvf $jar | grep pom.properties | perl -pe 's,.* ,,')
+
+    if [ -n "$pom" ]; then
+        jar xvf $jar $pom $props
+        source $props
+
+        mvn -Dmaven.repo.local=${MAVEN_REPO_LOCAL} install:install-file -DgroupId=$groupId -DartifactId=$artifactId -Dversion=$version -Dpackaging=jar -Dfile=$jar $@
+        mvn -Dmaven.repo.local=${MAVEN_REPO_LOCAL} install:install-file -DgroupId=$groupId -DartifactId=$artifactId -Dversion=$version -Dpackaging=pom -Dfile=$pom $@
+
+    else
+        echo "missing POM.xml in $jar"
+    fi
+done
+rm META-INF -r
