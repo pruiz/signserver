0d11b58fcca74b3c14881441a746021165e5e716
netmackan <netmackan@91c8134f-2a44-0410-ba45-dce2f532dbc6>
2018-12-15 13:27:56 +0000

    DSSINTER-264: DSSINTER-241: Fixed paths
    git-svn-id: https://signserver.svn.sourceforge.net/svnroot/signserver/trunk@9930 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- jenkins-files/pipelines/build_unit/deploytools/maven-manually-install.sh
+++ jenkins-files/pipelines/build_unit/deploytools/maven-manually-install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# Find directory for this script
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink -f "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 # Install DeployTools parent POM
 DPL_VERSION=2.2.1-SNAPSHOT
-mvn -Dmaven.repo.local=${MAVEN_REPO_LOCAL} install:install-file -DgroupId=org.signserver.deploytools -DartifactId=DeployTools -Dversion=${DPL_VERSION} -Dpackaging=pom -Dfile=DeployTools-${DPL_VERSION}-pom.xml $@
+mvn -Dmaven.repo.local=${MAVEN_REPO_LOCAL} install:install-file -DgroupId=org.signserver.deploytools -DartifactId=DeployTools -Dversion=${DPL_VERSION} -Dpackaging=pom -Dfile=${DIR}/DeployTools-${DPL_VERSION}-pom.xml $@
 
 # Install the DeployTools artifacts from JARS
 # Based on post by David Blevins: https://www.mail-archive.com/users@maven.apache.org/msg91297.html
-for jar in ./DeployTools-Common-${DPL_VERSION}.jar ./DeployTools-Maven-${DPL_VERSION}.jar; do
+for jar in ${DIR}/DeployTools-Common-${DPL_VERSION}.jar ${DIR}/DeployTools-Maven-${DPL_VERSION}.jar; do
     pom=$(jar tvf $jar | grep pom.xml | perl -pe 's,.* ,,')
     props=$(jar tvf $jar | grep pom.properties | perl -pe 's,.* ,,')
 
