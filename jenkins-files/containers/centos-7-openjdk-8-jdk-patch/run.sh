4b1d1407858d408bac3a2f578ae5c85aa5c9e881
malu9369 <malu9369@91c8134f-2a44-0410-ba45-dce2f532dbc6>
2019-03-13 13:10:09 +0000

    DSSINTER-276: Add container setup for JRE patch job. First attempt
    git-svn-id: https://signserver.svn.sourceforge.net/svnroot/signserver/trunk@10486 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ jenkins-files/containers/centos-7-openjdk-8-jdk-patch/run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -x
+echo "Building OpenJDK patch..."
+
+startDir="$(pwd)"
+
+cd /build/
+
+jdkDir=$(readlink -m $(dirname $(readlink -f $(which java)))/../..)
+echo "JDK directory: ${jdkDir}"
+
+unzip -qd src/ ${jdkDir}/src.zip
+
+patchJar() {
+    jarFile="$1"
+    patchFile="$2"
+    echo "Attempting to patch ${jarFile} using ${patchFile}..."
+    # Patch the JDK source files
+    patch -d src/ -p1 < ${patchFile}
+    mkdir out-${jarFile//.jar/}/
+    # Re-compile any file affected by the patch
+    cd src/
+    javac -d ../out-${jarFile//.jar/}/ -XDignore.symbol.file $(grep "diff " ../${patchFile} | awk '{print $4}' | sed 's/^b\///' | tr '\r\n' ' ') || exit 1
+    cd ..
+    # Make a local copy of the JAR from the JDK
+    cp "${jdkDir}/jre/lib/ext/${jarFile}" ${jarFile}
+    # Update local copy of the JAR with the patched and compiled files
+    jar uvf ${jarFile} -C out-${jarFile//.jar/}/ . || exit 1
+}
+
+patchJar sunec.jar     $HOME/binaries/openJDK8-sunec.patch
+patchJar sunpkcs11.jar $HOME/binaries/openJDK8-sunpkcs11.patch
+
+echo "Create a patch layer that shadows the original JARs when added as a Docker image layer..."
+cd /build/
+mkdir -p ./${jdkDir}/jre/lib/ext/
+mv *.jar ./${jdkDir}/jre/lib/ext/
+mkdir artifacts/
+
+artifactBaseName=$(basename $jdkDir)
+export XZ_OPT="-7e --threads=0"
+time tar -Ixz --directory=. -cvnf artifacts/openjdk-patch.tar.xz usr/lib/ || exit 1
+
+# Make a copy that we can attach as a build artifact
+cp artifacts/openjdk-patch.tar.xz artifacts/${artifactBaseName}-patch.tar.xz || exit 1
+
+ls -l artifacts/*
+
+cp -r artifacts "${startDir}/"
