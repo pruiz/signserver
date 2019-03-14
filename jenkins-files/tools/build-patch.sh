8be723be3fa1689f15d46f19b49daef0bb62fbe9
malu9369 <malu9369@91c8134f-2a44-0410-ba45-dce2f532dbc6>
2019-03-14 14:16:24 +0000

    DSSINTER-276: Remove debug cruft
    git-svn-id: https://signserver.svn.sourceforge.net/svnroot/signserver/trunk@10519 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- jenkins-files/tools/build-patch.sh
+++ jenkins-files/tools/build-patch.sh
@@ -25,38 +25,15 @@ patchJar() {
     jar uvf ${jarFile} -C out-${jarFile//.jar/}/ . || exit 1
 }
 
-echo "PWD: $PWD"
-echo "Contents of PWD"
-ls -ltra $PWD
-
-echo "Contents of $HOME"
-ls -ltra $HOME
-
-echo "Contents of $HOME/binaries"
-ls -ltra $HOME/binaries
-
-ls -ltra jenkins-files/containers/$CONTAINER/
-
 patchJar sunec.jar     jenkins-files/containers/$CONTAINER/binaries/openJDK8-sunec.patch
 patchJar sunpkcs11.jar jenkins-files/containers/$CONTAINER/binaries/openJDK8-sunpkcs11.patch
-
-echo "PWD after patch"
-ls -ltra $PWD
-
-ls -ltra $PWD/out-sunec
-ls -ltra $PWD/out-sunpkcs11/sun/security/pkcs11
 
 mkdir artifacts/
 
 artifactBaseName=$(basename $jdkDir)
-#export XZ_OPT="-7e --threads=0"
-#time tar -Ixz --directory=. -cvnf artifacts/openjdk-patch.tar.xz usr/lib/ || exit 1
 
 cp sunec.jar artifacts/sunec-${artifactBaseName}.jar
 cp sunpkcs11.jar artifacts/sunpkcs11-${artifactBaseName}.jar
-
-# Make a copy that we can attach as a build artifact
-#cp artifacts/openjdk-patch.tar.xz artifacts/${artifactBaseName}-patch.tar.xz || exit 1
 
 ls -l artifacts/*
 
