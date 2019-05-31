e4c39fbfaa5627c3ec9504c4018199c769ece245
Pablo Ruiz <pablo.ruiz@gmail.com>
2019-05-31 03:28:57 +0200

    Working implementation of EnhancedTimeStampSigner with qcStatements extension.

--- signserver/modules/SignServer-Module-TSA/src/java/org/signserver/module/tsa/EnhancedTimeStampSigner.java
+++ signserver/modules/SignServer-Module-TSA/src/java/org/signserver/module/tsa/EnhancedTimeStampSigner.java
@@ -6,8 +6,13 @@ package org.signserver.module.tsa;
 
 import java.io.IOException;
 import org.apache.log4j.Logger;
+import org.bouncycastle.asn1.ASN1Encoding;
+import org.bouncycastle.asn1.ASN1Sequence;
+import org.bouncycastle.asn1.ASN1EncodableVector;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.ASN1OctetString;
+import org.bouncycastle.asn1.DERSequence;
+import org.bouncycastle.asn1.DEROctetString;
 import org.bouncycastle.asn1.x509.Extension;
 import org.bouncycastle.asn1.x509.Extensions;
 import org.bouncycastle.asn1.x509.qualified.QCStatement;
@@ -30,8 +35,13 @@ public class EnhancedTimeStampSigner extends TimeStampSigner {
 	protected Extensions getAdditionalExtensions(final ProcessRequest request,
             RequestContext context)
             throws IOException {
-		QCStatement statement = new QCStatement(ID_ETSI_TSTS);
-		Extension extension = new Extension(Extension.qCStatements, false, ASN1OctetString.getInstance(statement));
+		final ASN1EncodableVector statements = new ASN1EncodableVector();
+		final QCStatement statement = new QCStatement(ID_ETSI_TSTS);
+		statements.add(statement);
+
+		//final byte[] encoded = value.toASN1Primitive().getEncoded(ASN1Encoding.DER);
+		final DEROctetString value = new DEROctetString(new DERSequence(statements));
+		Extension extension = new Extension(Extension.qCStatements, false, value);
 
         return new Extensions(new Extension[] { extension });
     }
